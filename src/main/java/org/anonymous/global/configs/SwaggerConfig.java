bed89ff33b191b3754fcf5cd6e099bc63736d986
PaB1010 <mudodd@naver.com>
2025-02-02 22:30:37 +0900

    초기 작업

--- src/main/java/org/anonymous/global/configs/SwaggerConfig.java
+++ src/main/java/org/anonymous/global/configs/SwaggerConfig.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info=@Info(title="은행 서비스", description = "계좌, 은행, 잔액 등 관리하는 서비스"))
+@OpenAPIDefinition(info=@Info(title="Bank API", description = "은행별 계좌 API 제공"))
 public class SwaggerConfig {
     
     @Bean
     public GroupedOpenApi apiGroup() {
         return GroupedOpenApi.builder()
-                .group("은행 서비스 v1")
+                .group("Bank API v1")
                 .pathsToMatch("/**")
                 .build();
     }
