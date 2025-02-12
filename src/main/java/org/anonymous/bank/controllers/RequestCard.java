9aaef1b54075178ccb8bc9969f572c5258843a5a
kks940104 <kks940104@gmail.com>
2025-02-12 23:22:14 +0900

    수정

--- src/main/java/org/anonymous/bank/controllers/RequestCard.java
+++ src/main/java/org/anonymous/bank/controllers/RequestCard.java
@@ -1,10 +1,12 @@
 package org.anonymous.bank.controllers;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.anonymous.bank.constants.CardType;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class RequestCard {
 
     // 카드 추천. -> 연회비, 카드종류 2개만 선택, 그리고 bankName 자동으로. 한도는 얘가 썼던 금액을 보고. 카테고리는 자동으로.
